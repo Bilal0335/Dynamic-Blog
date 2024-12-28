6aa66278b27331ab484a338755e78b7de7e64cbf
Bilal0335 <bilalhussain42201.com>
2024-12-28 13:26:44 +0500

    😄 smile 😄

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,9 +22,9 @@ const Navbar = () => {
           </a>
           <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
             <a className="mr-5 hover:text-gray-900 cursor-pointer">Home</a>
-            <a className="mr-5 hover:text-gray-900 cursor-pointer">About</a>
-            <a className="mr-5 hover:text-gray-900 cursor-pointer">Blog</a>
-            <a className="mr-5 hover:text-gray-900 cursor-pointer">Contact</a>
+            <a className="mr-5 hover:text-gray-900 cursor-pointer" href={"/about"}>About</a>
+            <a className="mr-5 hover:text-gray-900 cursor-pointer" href={"/blog"}>Blog</a>
+            <a className="mr-5 hover:text-gray-900 cursor-pointer" href={"/contact"}>Contact</a>
           </nav>
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer">
             Register
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1>Bilal</h1>
+      {/* <h1>Bilal</h1> */}
     </>
   );
 }
